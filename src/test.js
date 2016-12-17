2e7bd20d3fcc531e73d6bfb0d17d44b836ffa0af
Ryan Batchelder <ryanbatch@gmail.com>
2016-12-16 21:41:48 -0500

    Adding tests and travis config

--- src/test.js
+++ src/test.js
@@ -1,5 +1,17 @@
 var bus_timer = require('./bus_timer.js');
-console.log(bus_timer.getPrediction);
-bus_timer.getPrediction("426", "market street at commuter rail", undefined, 'wX9NwuHnZU2ToO7GmGR9uw').then(function(prediction) {
-    console.log(prediction);
-});
+var assert = require('assert');
+
+describe('bus timer tests', function() {
+    it ('should return a time with a stop, route, and no direction', function(done) {        
+        bus_timer.getPrediction("426", "market street at commuter rail", undefined, process.env['mbta_api']).then(function(prediction) {
+            assert(prediction !== undefined);
+            done();
+        });
+    });
+    it ('should return a time with a stop, route, and direction', function(done) {        
+        bus_timer.getPrediction("426", "cliftondale square", "outbound", process.env['mbta_api']).then(function(prediction) {
+            assert(prediction !== undefined);
+            done();
+        });
+    });
+})
