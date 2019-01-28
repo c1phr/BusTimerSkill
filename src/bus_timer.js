216f9f1bc49c82ea80c475f0f5fe78265eb72f3c
Ryan Batchelder <ryanbatch@gmail.com>
2019-01-27 21:34:46 -0500

    Migrate to MBTA v3 API

--- src/bus_timer.js
+++ src/bus_timer.js
@@ -1,16 +1,22 @@
 'use strict';
 var request = require('request-promise-native');
 
-const mbtaApi = 'http://realtime.mbta.com/developer/api/v2/';
+const mbtaApi = 'https://api-v3.mbta.com/';
 var api_key;
 
 var getPrediction = function(route, stop, direction, apiKey) {
     console.log("Get Prediction called with Route: " + route + " Stop: " + stop + " Direction: " + direction);    
     api_key = apiKey;
-    var routeId = getRouteId(route);    
-    return getStopId(routeId, stop, direction).then(function(stopId) {        
-        return getTime(routeId, stopId);
-    });    
+    return getRoute(route).then(function(route) {        
+        var routeId = route.id    
+        var directionIdx = findDirectionId(direction, route.attributes.direction_names);
+        return getStopId(routeId, stop, directionIdx).then(function(stopId) {        
+            return getTime(routeId, stopId);
+        });    
+    }).catch(function(exception) {
+        console.error(exception);
+        return undefined;
+    });
 }
 
 var getTime = function(route, stop) {
@@ -21,69 +27,75 @@ var getTime = function(route, stop) {
         throw Error("STOP");
     }
     var predictionOpts = {
-        uri: mbtaApi + 'predictionsbystop',
+        uri: mbtaApi + 'predictions',
         qs: {            
-            stop: stop            
-        }        
+            "filter[stop]": stop
+        }
     };
     return httpGet(predictionOpts).then(function(predictionData) {
-        if (predictionData.mode === undefined) {
+        console.log(predictionData.data[0].attributes)
+        if (predictionData.data === undefined || predictionData.data.length === 0 || predictionData.data[0].attributes.arrival_time === undefined) {
             return undefined;
         }
-        var predictedMinutes = Math.floor(predictionData.mode[0].route[0].direction[0].trip[0].pre_away / 60);
-        return predictedMinutes;
+        var predictedArrival = convertDateToUTC(new Date(predictionData.data[0].attributes.arrival_time));
+        var now = convertDateToUTC(new Date());
+        var diffDate = Math.abs(predictedArrival - now);
+        return Math.floor((diffDate/1000)/60);
     });
 }
 
-var getRouteId = function(routeName) {
-    if (isNumber(routeName)) { // Short circuit for bus route inputs (id === route number for MBTA)
-        return parseInt(routeName);
-    }
+var getRoute = function(routeName) {    
     var routeOpts = {
-        uri: mbtaApi + 'routes'        
+        uri: mbtaApi + 'routes',
+        qs: {}
     }
-    return httpGet(routeOpts).then(function(routeData) {        
+    if (isNumber(routeName)) { // Short circuit for bus route inputs (id === route number for MBTA)
+        routeOpts.uri += '/' + parseInt(routeName);
+    }
+    return httpGet(routeOpts).then(function(routeData) {
         var possibleRoutes = [];
-        routeData.forEach(function(modeElem) {
-            modeElem.route.forEach(function(routeElem) {
-                var matchConfidence = fuzzyMatch(routeName, routeElem.route_name);
-                if (matchConfidence > 0) {
-                    possibleRoutes.push({route: routeElem, confidence: matchConfidence});
-                }
+        if (Array.isArray(routeData.data)) {
+            routeData.data.forEach(function(modeElem) {
+                modeElem.route.forEach(function(routeElem) {
+                    var matchConfidence = fuzzyMatch(routeName, routeElem.attributes.short_name);
+                    if (matchConfidence > 0) {
+                        possibleRoutes.push({route: routeElem, confidence: matchConfidence});
+                    }
+                });
             });
-        });
-
+        }
+        else {            
+            possibleRoutes.push({route: routeData.data, confidence: 1000})
+        }
         possibleRoutes.sort(function(a, b) {
             return b.confidence - a.confidence;
-        });
-        return possibleRoutes.length > 0 ? possibleRoutes[0].route.route_id : undefined;
+        });        
+        var route = possibleRoutes.length > 0 ? possibleRoutes[0].route : undefined;
+        return route
     });
 }
 
-var getStopId = function(route, stopName, directionName) {
+var getStopId = function(route, stopName, directionIdx) {
     var stopOpts = {
-        uri: mbtaApi + 'stopsbyroute',
+        uri: mbtaApi + 'stops',
         qs: {
-            route: route
+            "filter[route]": route,
+            "filter[direction_id]": directionIdx
         }
     };
     stopName = stopName.toLowerCase();
-    return httpGet(stopOpts).then(function(directionData) {
+    return httpGet(stopOpts).then(function(stops) {
         var possibleStops = [];        
-        directionData.direction.forEach(function(direction) {                   
-            if (directionName === undefined || direction.direction_name.toLowerCase() === directionName.toLowerCase()) {
-                direction.stop.forEach(function(stop) {
-                    var matchConfidence = fuzzyMatch(stopName, stop.stop_name.toLowerCase());
-                    if (matchConfidence > 0) {
-                        possibleStops.push({stop: stop, confidence: matchConfidence});
-                    }
-                });
-            }
+        stops.data.forEach(function(stop) {                   
+            var matchConfidence = fuzzyMatch(stopName, stop.attributes.name.toLowerCase());
+                if (matchConfidence > 0) {
+                    possibleStops.push({stop: stop, confidence: matchConfidence});
+                }
         });        
         possibleStops.sort(function(a, b) {
             return b.confidence - a.confidence;
-        });                        
-        return possibleStops.length > 0 ? possibleStops[0].stop.stop_id : undefined;
+        });                
+        return possibleStops.length > 0 ? possibleStops[0].stop.id : undefined;
     });
 }
 
@@ -101,6 +113,12 @@ var fuzzyMatch = function(source, toMatch) {
     return confidence / matchSplit.length;
 }
 
+var findDirectionId = function(directionIntent, directionArray) {
+    var dirIntent = directionIntent.toLowerCase();
+    var dirArray = directionArray.map(function(dir) { return dir.toLowerCase(); });
+    return dirArray.indexOf(dirIntent);
+}
+
 var httpGet = function(opts, callback) {
     opts.qs.api_key = api_key;
     opts.qs.format = 'json';
@@ -110,6 +128,10 @@ var httpGet = function(opts, callback) {
 
 var isNumber = function(input) {
     return !isNaN(parseInt(input)) && isFinite(input);
+}
+
+var convertDateToUTC = function(date) { 
+    return new Date(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate(), date.getUTCHours(), date.getUTCMinutes(), date.getUTCSeconds()); 
 }
 
 exports.getPrediction = getPrediction;
